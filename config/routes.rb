a5aa897b37510e5b73b4afc3417231d8d5561604
Michael Ball <cycomachead@gmail.com>
2015-03-09 23:49:25 -0700

    cleanup routes to use subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  namespace :api do
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/'  do
     namespace :v1 do
-      resources :user
+      resources :user do
+        resources :projects
+      end
     end
   end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
