f18194c40d3a2a7aeb8b107dfe93b269f777bc09
Michael Ball <cycomachead@gmail.com>
2015-03-10 23:14:10 -0700

    subdomains suck for testing so we will figure this out later...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdomain: 'api' }, path: '/'  do
+  # NOTE: We should probably use a subdomain in the future, add:
+  # constraints: { subdomain: 'api' }, path: '/'
+  namespace :api, defaults: { format: :json }  do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users do
