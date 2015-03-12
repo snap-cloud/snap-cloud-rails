7bed2b98ff8aac4c0e21d4ed3ddd96c467b436fc
Michael Ball <cycomachead@gmail.com>
2015-03-12 00:15:26 -0700

    ooopsie. SyntaxError

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,3 @@
 class Project < ActiveRecord::Base
-  attr_accessible :last_modified, :owner, :is_public, :contents, :thumbnail, :notes, :title,
+  attr_accessible :last_modified, :owner, :is_public, :contents, :thumbnail, :notes, :title
 end
