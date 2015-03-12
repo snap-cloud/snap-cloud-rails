03fddc62cf085df46af4696d5f1f2ca8966bdc1c
Michael Ball <cycomachead@gmail.com>
2015-03-12 03:01:49 -0700

    csrf test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # FIXME --- we should fix this.
-  skip_before_filter :verify_authenticity_token
+  # skip_before_filter :verify_authenticity_token
 
-  # protect_from_forgery with: :exception => :create
+  protect_from_forgery with: :exception => :create
 end
