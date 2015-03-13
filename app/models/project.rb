4dd77b6df8089f19ff48e79f2d5d079204bd2190
Linda Lee <lindanaeunlee@eecs.berkeley.edu>
2015-03-12 17:22:01 -0700

    added attr_accessible to projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   # skip_before_filter :verify_authenticity_token
-
+  attr_accessible :title, :notes, :thumbnail, :contents, :is_public, :owner, 
+  :last_modified, :created_at, :updated_at
 end
