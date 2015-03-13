607b2731d6d4fa393bd527aee28601eb9cac410c
Linda Lee <lindanaeunlee@eecs.berkeley.edu>
2015-03-12 17:59:40 -0700

    fixed: project has attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,10 @@
 class Project < ActiveRecord::Base
   # skip_before_filter :verify_authenticity_token
-  attr_accessible :title, :notes, :thumbnail, :contents, :is_public, :owner, 
-  :last_modified, :created_at, :updated_at
+
+  ### LL : attr_accessible not supported in Rails 4.4.1; 
+  ### 	   so you use "strong parameters" instead; 
+  ###      see method user_params in projects_controller.rb
+  #attr_accessible :title, :notes, :thumbnail, :contents, :is_public, :owner, 
+  #:last_modified, :created_at, :updated_at
+  
 end
