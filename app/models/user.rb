03d6e94c1ea0e123f6409b2efaad112ebfe1dd1d
Arjun Baokar <arjunbaokar@gmail.com>
2015-03-13 18:25:33 -0700

    added a lot of simple_token_auth stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # use simple_token_authentication on users
+  acts_as_token_authenticatable
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
