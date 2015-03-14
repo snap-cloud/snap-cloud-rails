4d2ef332e95c2387fa2a0f2efc68777280b5f4ed
Linda Lee <lindanaeunlee@eecs.berkeley.edu>
2015-03-13 20:30:09 -0700

    make foreign key the owner column, which points to id in users

--- db/migrate/20150314024623_associating_projects_and_users.rb
+++ db/migrate/20150314024623_associating_projects_and_users.rb
@@ -1,6 +1,5 @@
 class AssociatingProjectsAndUsers < ActiveRecord::Migration
   def change
-  	add_column :projects, :user_id, :integer
-  	add_foreign_key :projects, :users, column: :user_id, primary_key: "id"
+  	add_foreign_key :projects, :users, column: :owner, primary_key: "id"
   end
 end
