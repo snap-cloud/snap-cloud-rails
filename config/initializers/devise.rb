53a0b2677dabc9c0aabeb37ca6b9fe1e63bc6c1d
Arjun Baokar <arjunbaokar@gmail.com>
2015-03-13 18:31:32 -0700

    removed s_t_a initializer and added json to devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -226,6 +226,7 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ["/", :json]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
 
