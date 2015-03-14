11931729da4557848ae444d08a3df8f05daffb55
alecguertin <alecguertin@berkeley.edu>
2015-03-13 18:18:54 -0700

    projects curl working

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,5 @@
 class Project < ActiveRecord::Base
   # skip_before_filter :verify_authenticity_token
 
-  attr_accessor :title
-  attr_accessor :notes
-  attr_accessor :thumbnail
-  attr_accessor :contents
-  attr_accessor :is_public
-  attr_accessor :owner
-  attr_accessor :last_modified
-  attr_accessor :created_at
-  attr_accessor :updated_at
 
 end
