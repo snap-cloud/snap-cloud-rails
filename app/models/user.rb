7fe4afc0beec04a9400813352fd056a76d3bf8c3
alecguertin <alecguertin@berkeley.edu>
2015-04-03 04:30:41 -0700

    added signup fields to users, set up template for user profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   # use simple_token_authentication on users
   acts_as_token_authenticatable
 
+  validates_length_of :about_me, :maximum => 500, :allow_blank => true
+
   # A user has many projects, and deleting user deletes projects of that user
   # FIXME -- how do we handle projects with multiple owners?
   has_many :projects, dependent: :destroy
