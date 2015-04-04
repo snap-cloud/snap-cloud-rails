b34458cc2a8f0ce8ac37d26319c4533b2fb2d2c3
Linda Lee <lindanaeunlee@eecs.berkeley.edu>
2015-04-03 17:58:49 -0700

    created announcements model and made news feed a partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403033622) do
+ActiveRecord::Schema.define(version: 20150404003354) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.text     "source"
+    t.integer  "source_id"
+    t.string   "to_whom"
+    t.text     "text"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.text     "title"
@@ -44,16 +54,15 @@ ActiveRecord::Schema.define(version: 20150403033622) do
     t.datetime "last_modified"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -61,6 +70,10 @@ ActiveRecord::Schema.define(version: 20150403033622) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.text     "about_me"
+    t.date     "birthdate"
+    t.string   "username"
+    t.boolean  "parent_permission",      default: false
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
---
+++ spec/factories/announcements.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :announcement do
+    source "MyText"
+source_id 1
+to_whom "MyString"
+text "MyText"
+time "2015-04-03 17:33:54"
+  end
+
+end
