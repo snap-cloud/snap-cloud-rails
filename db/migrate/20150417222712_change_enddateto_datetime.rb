ffd7aa258378f52b112dd4cf7979cddfb0284b39
Ackpacket <scampos1030@gmail.com>
2015-04-17 15:34:49 -0700

    Fix typo in migration file

--- db/migrate/20150417222712_change_enddateto_datetime.rb
+++ db/migrate/20150417222712_change_enddateto_datetime.rb
@@ -1,5 +1,5 @@
 class ChangeEnddatetoDatetime < ActiveRecord::Migration
   def change
-    change_column :Assignments, :end_date,  :datetime
+    change_column :Assignments, :due_date,  :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417222306) do
+ActiveRecord::Schema.define(version: 20150417222712) do
 
   create_table "Assignments", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.date     "start_date"
-    t.date     "due_date"
+    t.datetime "due_date"
     t.integer  "course_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
