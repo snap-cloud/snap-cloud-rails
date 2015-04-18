5427777c9b1cf0adc0549641b0f0d5c2bb376301
Michael Ball <cycomachead@gmail.com>
2015-04-18 02:23:45 -0700

    A Bunch of whitespace cleanup (rspec)

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,28 +4,35 @@ describe Course do
     describe "#students" do
         before(:each) do
 
-            @course = Course.create(title: "CS 169 - Software Engineering", description: "This is a course on engineering software.  In the spring 2015 semester, the focus will be on engineering software as a service.", website: "http://google.com", startdate: 10.days.ago, enddate: 10.days.from_now)
+            @course = Course.create(title: "CS 169 - Software Engineering",
+                                    description: "This is a course on engineering software.  In the spring 2015 semester, the focus will be on engineering software as a service.",
+                                    website: "http://google.com",
+                                    startdate: 10.days.ago,
+                                    enddate: 10.days.from_now)
 
-
-
-            @alec = User.create(email: "alec@alec.com", password: "passwordhungary")
-            @jason = User.create(email: "jason@jason.com", password: "passwordhungary")
-            @arjun = User.create(email: "arjun@arjun.com", password: "passwordhungary")
-            @linda = User.create(email: "linda@linda.com", password: "passwordhungary")
-            @steve = User.create(email: "steve@steve.com", password: "passwordhungary")
-            @michael = User.create(email: "michael@michael.com", password: "passwordhungary")
+            @alec = User.create(email: "alec@alec.com",
+                                password: "passwordhungary")
+            @jason = User.create(email: "jason@jason.com",
+                                password: "passwordhungary")
+            @arjun = User.create(email: "arjun@arjun.com",
+                                password: "passwordhungary")
+            @linda = User.create(email: "linda@linda.com",
+                                password: "passwordhungary")
+            @steve = User.create(email: "steve@steve.com",
+                                password: "passwordhungary")
+            @michael = User.create(email: "michael@michael.com",
+                                password: "passwordhungary")
 
             @course.addUser(@alec, :student)
             @course.addUser(@jason, :student)
             @course.addUser(@arjun, :student)
 
             @course.addUser(@linda, :teacher)
-            
-
         end
 
         it "Should return all the students in the course" do
             SIDS = @course.students.map(&:id)
+            puts SIDS
             expect(SIDS).to include @alec.id
             expect(SIDS).to include @jason.id
             expect(SIDS).to include @arjun.id
