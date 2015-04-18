3b26fc3860e7b1a0c87daccec0ac662982e83b57
Jason Wang <jason.wang@berkeley.edu>
2015-04-17 20:21:11 -0700

    writing submissions controller

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,12 @@
 class Assignment < ActiveRecord::Base
   belongs_to :course
+
+  def submit(project_id, comments)
+  	proj = Project.find(project_id)
+  	subCopy = proj.dup
+  	subCopy.last_modified = proj.last_modified
+  	subCopy.read_only = true
+  	subCopy.save
+  	Submission.create(title: subCopy.title, assignment_id: assignment_id, comments: comments, project_id: subCopy.id)
+  end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,3 @@
-class Assignment < ActiveRecord::Base
+class Submission < ActiveRecord::Base
   belongs_to :assignment
 end
