e3fc53c1b2149fc4143453c20c6bbac4eb1067ce
Ackpacket <scampos1030@gmail.com>
2015-04-17 18:11:48 -0700

    Change submissions table, change course show so links are ID'd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417222712) do
+ActiveRecord::Schema.define(version: 20150418005633) do
 
   create_table "Assignments", force: :cascade do |t|
     t.string   "title"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150417222712) do
     t.datetime "last_modified"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "comments"
   end
 
   create_table "users", force: :cascade do |t|
