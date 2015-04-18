8760c33cab2b7537779b46be35a27e3f1b544883
Jason Wang <jason.wang@berkeley.edu>
2015-04-17 20:21:11 -0700

    writing submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,6 +1,38 @@
 class SubmissionsController < ApplicationController
-    def new
-        @user = current_user
-        @assignment = Assignment.find_by(params[:assignment_id])
+  before_filter :userLoggedIn
+  before_filter :assignmentExists
+  before_filter :canSubmit
+
+    def create
+      @assignment.submit(submission_params)
     end
+
+    def submission_params
+      params.require(:submission).permit(:project_id, :comments)
+    end
+
+    def userLoggedIn
+      if getCurrentUser.nil?
+        redirect_to login_path and return
+      else
+        @user = getCurrentUser
+      end
+    end
+
+    def assignmentExists
+      if !Assignment.exists?(params[:assignment_id])
+        render file: "#{Rails.root}/public/404.html", layout: false, status: 404 and return
+      else
+        @assignment = Assignment.find(params[:assignment_id])
+      end
+    end
+
+    def canSubmit
+      course = @assignment.course
+      if not course.student_ids.include? @user.id
+        render file: "#{Rails.root}/public/401.html", layout: false, status: 401 and return
+      end
+    end
+
+
 end
