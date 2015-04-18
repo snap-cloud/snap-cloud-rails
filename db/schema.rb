91d97e9567de359a253dd6618a7075e7c01be2da
Ackpacket <scampos1030@gmail.com>
2015-04-17 19:21:00 -0700

    Changed the submissions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418005633) do
+ActiveRecord::Schema.define(version: 20150418021123) do
 
   create_table "Assignments", force: :cascade do |t|
     t.string   "title"
@@ -70,15 +70,11 @@ ActiveRecord::Schema.define(version: 20150418005633) do
 
   create_table "submissions", force: :cascade do |t|
     t.string   "title"
-    t.text     "notes"
-    t.binary   "thumbnail"
-    t.text     "contents"
-    t.boolean  "is_public"
-    t.integer  "owner"
-    t.datetime "last_modified"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "assignment_id"
+    t.integer  "project_id"
     t.text     "comments"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
