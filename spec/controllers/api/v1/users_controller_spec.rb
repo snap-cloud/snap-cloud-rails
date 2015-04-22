705946d11a802ff33af9b3ced7570f8facd082d9
Michael Ball <cycomachead@gmail.com>
2015-04-22 01:15:27 -0700

    consistency in code style..ish [ci skip]

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -9,19 +9,16 @@ describe Api::V1::UsersController do
 
   describe "GET #show" do
     before(:each) do
-      #Added by steve for alec
       fakeuser = double('user')
       allow(request.env['warden']).to receive(:authenticate!).and_return(fakeuser)
       allow(controller).to receive(:current_user).and_return(fakeuser)
-
-
 
       @user = FactoryGirl.create :user
       get :show, id: @user.id, format: :json
 
     end
 
-    it "returns the information about a reporter on a hash" do
+    it "renders json with the users info" do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
@@ -30,12 +27,10 @@ describe Api::V1::UsersController do
   end
 
   describe "POST #create" do
-
-    context "when is successfully created" do
+    context "when a user is successfully created" do
       before(:each) do
         @user_attrs = FactoryGirl.attributes_for :user
 
-        # Added by steve for alec
         fakeuser = double('user')
         allow(request.env['warden']).to receive(:authenticate!).and_return(fakeuser)
         allow(controller).to receive(:current_user).and_return(fakeuser)
@@ -51,13 +46,12 @@ describe Api::V1::UsersController do
       it { should respond_with 201 }
     end
 
-    context "when is not created" do
+    context "when a user is not created" do
       before(:each) do
-        #notice I'm not including the email
+        # notice I'm not including the email
         @invalid_user_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
 
-        #Added by steve for alec
         fakeuser = double('user')
         allow(request.env['warden']).to receive(:authenticate!).and_return(fakeuser)
         allow(controller).to receive(:current_user).and_return(fakeuser)
@@ -66,12 +60,12 @@ describe Api::V1::UsersController do
         post :create, { user: @invalid_user_attributes }, format: :json
       end
 
-      it "renders an errors json" do
+      it "renders json with errors" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on why the user could not be created" do
+      it "renders json errors for why the user could not be created" do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
@@ -84,14 +78,11 @@ describe Api::V1::UsersController do
     before(:each) do
       @user = FactoryGirl.create :user
 
-      #Added by steve for alec
       fakeuser = double('user')
       allow(request.env['warden']).to receive(:authenticate!).and_return(fakeuser)
       allow(controller).to receive(:current_user).and_return(fakeuser)
 
       delete :destroy, { id: @user.id }, format: :json
-
-
     end
 
     it { should respond_with 204 }
@@ -99,8 +90,7 @@ describe Api::V1::UsersController do
   end
 
   describe "PUT/PATCH #update" do
-
-    context "when is successfully updated" do
+    context "when a user successfully updated" do
       before(:each) do
         @user = FactoryGirl.create :user
 
