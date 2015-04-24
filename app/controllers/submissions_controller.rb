09f0ee2c4795b6fcfbf1e1814c7c86379049a232
Jason Wang <jason.wang@berkeley.edu>
2015-04-24 03:32:04 -0700

    more refactoring filters

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,8 @@
 class SubmissionsController < ApplicationController
   before_filter :userLoggedIn
+  before_filter :courseExists
   before_filter :assignmentExists
-  before_filter :canSubmit
+  before_filter :partOfCourse
 
     def create
       @submission = submission_params
@@ -12,33 +13,6 @@ class SubmissionsController < ApplicationController
 
     def submission_params
       params.require(:submission).permit(:project_id, :comments)
-    end
-
-    def userLoggedIn
-      if getCurrentUser.nil?
-        redirect_to login_path and return
-      else
-        @user = getCurrentUser
-      end
-    end
-
-    def assignmentExists
-      if !Assignment.exists?(params[:assignment_id])
-        render file: "#{Rails.root}/public/404.html", layout: false, status: 404 and return
-      else
-        @assignment = Assignment.find(params[:assignment_id])
-      end
-    end
-
-    def canSubmit
-      course = @assignment.course
-      if not course.student_ids.include? @user.id
-        render file: "#{Rails.root}/public/401.html", layout: false, status: 401 and return
-      end
-    end
-
-    def getCurrentUser
-      current_user
     end
 
 end
