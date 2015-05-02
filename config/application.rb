b1edcb73ed76d98e2d9129f945fbaf2fc10f34bb
Michael Ball <cycomachead@gmail.com>
2015-05-02 04:06:40 -0700

    Fix the assets pipeline I hope!

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module SnapCloud
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    # -- all .rb files in that directory are automatically loaded.    
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
