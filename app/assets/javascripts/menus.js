881a603ce68a00d11a29eeaa330f30fe2de54778
Michael Ball <cycomachead@gmail.com>
2015-05-09 00:17:56 -0700

    The header is now more responsive. Not perfect...but better...

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,38 +1,44 @@
-(function (window, document) {
-var menu = document.getElementById('menu'),
-    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+// Slighty modified from the PureCSS website.
+// The controls the hamburger menu on small screens.
+$(document).ready(function() {
+    var menu = document.getElementById('menu'),
+        toggle = document.getElementById('toggle'),
+        WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
 
-function toggleHorizontal() {
-    [].forEach.call(
-        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
-        function(el){
-            el.classList.toggle('pure-menu-horizontal');
+    function toggleHorizontal() {
+        [].forEach.call(
+            document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+            function(el){
+                el.classList.toggle('pure-menu-horizontal');
+            }
+        );
+    };
+
+    function toggleMenu() {
+        // set timeout so that the panel has a chance to roll up
+        // before the menu switches states
+        if (menu.classList.contains('open')) {
+            setTimeout(toggleHorizontal, 500);
         }
-    );
-};
+        else {
+            toggleHorizontal();
+        }
+        menu.classList.toggle('open');
+        document.getElementById('toggle').classList.toggle('x');
+    };
 
-function toggleMenu() {
-    // set timeout so that the panel has a chance to roll up
-    // before the menu switches states
-    if (menu.classList.contains('open')) {
-        setTimeout(toggleHorizontal, 500);
+    function closeMenu() {
+        if (menu.classList.contains('open')) {
+            toggleMenu();
+        }
     }
-    else {
-        toggleHorizontal();
-    }
-    menu.classList.toggle('open');
-    document.getElementById('toggle').classList.toggle('x');
-};
 
-function closeMenu() {
-    if (menu.classList.contains('open')) {
+    // Simple error handling incase something weird happens...
+    if (!menu || !toggle) { return; }
+    
+    toggle.addEventListener('click', function (e) {
         toggleMenu();
-    }
-}
+    });
 
-document.getElementById('toggle').addEventListener('click', function (e) {
-    toggleMenu();
+    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
 });
-
-window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-})(this, this.document);
