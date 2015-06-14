29c9643ee9f6c5dad47ed89e78379ed96d20d382
Michael Ball <cycomachead@gmail.com>
2015-06-14 01:38:08 -0700

    now fix my stupid mistake and the button works

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,43 +1,44 @@
-// // Slighty modified from the PureCSS website.
-// // The controls the hamburger menu on small screens.
-// $(document).ready(function() {
-//     (function (window, document) {
-//     var menu = document.getElementById('menu'),
-//         WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
-//
-//     function toggleHorizontal() {
-//         [].forEach.call(
-//             document.getElementById('menu').querySelectorAll('.custom-can-transform'),
-//             function(el){
-//                 el.classList.toggle('pure-menu-horizontal');
-//             }
-//         );
-//     };
-//
-//     function toggleMenu() {
-//         // set timeout so that the panel has a chance to roll up
-//         // before the menu switches states
-//         if (menu.classList.contains('open')) {
-//             setTimeout(toggleHorizontal, 500);
-//         }
-//         else {
-//             toggleHorizontal();
-//         }
-//         menu.classList.toggle('open');
-//         document.getElementById('toggle').classList.toggle('x');
-//     };
-//
-//     function closeMenu() {
-//         if (menu.classList.contains('open')) {
-//             toggleMenu();
-//         }
-//     }
-//
-//     document.getElementById('toggle').addEventListener('click', function (e) {
-//         toggleMenu();
-//     });
-//
-//     window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-//     })(window, window.document);
-//
-// });
+// Slighty modified from the PureCSS website.
+// The controls the hamburger menu on small screens.
+$(document).ready(function() {
+    (function (window, document) {
+    var menu = document.getElementById('menu'),
+        toggle = document.getElementById('toggle'),
+        WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+
+    function toggleHorizontal() {
+        [].forEach.call(
+            menu.querySelectorAll('.custom-can-transform'),
+            function(el){
+                el.classList.toggle('pure-menu-horizontal');
+            }
+        );
+    };
+
+    function toggleMenu() {
+        // set timeout so that the panel has a chance to roll up
+        // before the menu switches states
+        if (menu.classList.contains('open')) {
+            setTimeout(toggleHorizontal, 500);
+        }
+        else {
+            toggleHorizontal();
+        }
+        menu.classList.toggle('open');
+        toggle.classList.toggle('x');
+    };
+
+    function closeMenu() {
+        if (menu.classList.contains('open')) {
+            toggleMenu();
+        }
+    }
+
+    toggle.addEventListener('click', function (e) {
+        toggleMenu();
+    });
+
+    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+    })(window, window.document);
+
+});
